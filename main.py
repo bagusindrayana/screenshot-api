eca50e1d502e1fb1fc5f36bed42475f3f8ef91bf
bagusindrayana <bagusindrayanaindo@gmail.com>
2025-09-10 18:08:24 +0800

    host and port dari env

--- main.py
+++ main.py
@@ -239,4 +239,6 @@ async def startup_event():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000)) 
+    host = os.getenv("HOST", "0.0.0.0")
+    uvicorn.run(app, host=host, port=port)
